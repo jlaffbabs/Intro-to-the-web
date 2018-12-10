93711374c8261237576fa8c30dbdd69a5a5be3df
Jacques <jacques.laffy@gmail.com>
2018-12-10 16:53:03 +0000

    Erb file

--- app.rb
+++ app.rb
@@ -9,7 +9,5 @@ get '/route' do
 end
 
 get '/cat' do
-  "<div style = 'border: 2px dashed red; width: 454px'>
-  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
-  </div>"
+  erb(:index)
 end
